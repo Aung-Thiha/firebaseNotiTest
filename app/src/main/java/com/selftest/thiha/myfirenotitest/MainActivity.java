956b5c03d310a5db498be75d52c8922f13c7440b
PC - 010 <pc-010@PCs-MacBook-Air.lan>
2017-03-16 10:34:20 +0630

    noti fine

--- app/src/main/java/com/selftest/thiha/myfirenotitest/MainActivity.java
+++ app/src/main/java/com/selftest/thiha/myfirenotitest/MainActivity.java
@@ -2,6 +2,19 @@ package com.selftest.thiha.myfirenotitest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +22,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseMessaging.getInstance().subscribeToTopic("androids");
+
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference();
+
+
+
+
+
+
+
+
     }
+
 }
--- app/src/main/java/com/selftest/thiha/myfirenotitest/MyFirebaseMessagingService.java
+++ app/src/main/java/com/selftest/thiha/myfirenotitest/MyFirebaseMessagingService.java
@@ -1,13 +1,28 @@
 package com.selftest.thiha.myfirenotitest;
 
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 
 /**
  * Created by Thiha on 11/2/2016.
@@ -21,14 +36,61 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated.
         Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        //Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+        Map<String,String> data = remoteMessage.getData();
+        sendNotification(data);
+//        if (remoteMessage.getNotification() != null) {
+//            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+//
+//            String title = remoteMessage.getNotification().getTitle();
+//            String body = remoteMessage.getNotification().getBody();
+//
+//
+//            DatabaseReference ref= FirebaseDatabase.getInstance().getReference();
+//            Map noti = new HashMap<>();
+//            noti.put("title",title);
+//            noti.put("body",body);
+//            ref.child("NewNotis").push().setValue(noti);
+//        }
 
 
-        Intent intent = new Intent(this,MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        Intent intent = new Intent(this,MainActivity.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
 
 
     }
+
+    private void sendNotification(Map<String, String>  data) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0 /* request code */, intent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long[] pattern = {500,500,500,500,500};
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.noti16)
+                .setLargeIcon(BitmapFactory.decodeResource(this.getResources(),
+                        R.drawable.noti))
+                .setContentTitle(data.get("title"))
+                .setContentText(data.get("body"))
+                .setAutoCancel(true)
+                .setVibrate(pattern)
+                .setLights(Color.BLUE,1,1)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        Random r = new Random();
+        int notiId = r.nextInt();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(notiId, notificationBuilder.build());
+
+    }
+
 }
